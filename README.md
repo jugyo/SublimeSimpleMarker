dfa4964e2ee0ca96ea71dc224cf5db03d22bda6a
Kazuyuki Kohno <kkohno@twitter.com>
2013-09-27 18:49:03 +0900

    initiali commit

---
+++ README.md
@@ -0,0 +1,20 @@
+SublimeSimpleMarker
+========
+
+Sublime Text plugin to manage MARKERS.
+
+## Installation
+
+```
+$ cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+$ git clone https://github.com/jugyo/SublimeSimpleMarker.git SimpleMarker
+```
+
+## Key Binding
+
+```javascript
+[
+  { "keys": ["super+shift+k", "super+shift+m"], "command": "simple_marker", "args": {"mode": "list"} },
+  { "keys": ["super+shift+k", "super+shift+n"], "command": "simple_marker", "args": {"mode": "add"} },
+]
+```
---
+++ simple_marker.py
@@ -0,0 +1,96 @@
+import sublime, sublime_plugin
+import os
+
+SETTING_FILE_NAME = "SimpleMarker.sublime-settings"
+
+class SimpleMarkerSetting:
+    def load_markers(self):
+        settings = self.load_settings()
+        markers = settings.get('markers')
+        if markers == None:
+            markers = []
+        return markers
+
+    def save_markers(self, markers):
+        self.load_settings().set('markers', markers)
+        self.save_settings()
+
+    def load_settings(self):
+        return sublime.load_settings(SETTING_FILE_NAME)
+
+    def save_settings(self):
+        sublime.save_settings(SETTING_FILE_NAME)
+
+class SimpleMarkerListener(sublime_plugin.EventListener, SimpleMarkerSetting):
+    def __init__(self):
+        self.refresh(sublime.active_window().active_view())
+
+    def on_load(self, view):
+        self.refresh(view)
+
+    def on_activated(self, view):
+        self.refresh(view)
+
+    def on_modified(self, view):
+        self.refresh(view)
+
+    def refresh(self, view):
+        markers = self.load_markers()
+        regions = []
+        for marker in markers:
+            regions += view.find_all(marker)
+
+        view.add_regions("SimpleMarker", regions, "mark", "dot",
+            # sublime.DRAW_EMPTY)
+            sublime.DRAW_NO_FILL)
+
+class SimpleMarkerCommand(sublime_plugin.TextCommand, SimpleMarkerSetting):
+    def run(self, edit, mode):
+        self.window = self.view.window()
+
+        if mode == "add":
+            self.add()
+        elif mode == "list":
+            self.list()
+
+    def add(self):
+        sel = self.view.sel()[0]
+        if not sel.begin() == sel.end():
+            self.new_marker(self.view.substr(sel).strip())
+        else:
+            self.new_marker()
+
+    def add_marker(self, text):
+        markers = self.load_markers()
+        markers.insert(0, text)
+        self.save_markers(list(set(markers)))
+        self.list()
+
+    def new_marker(self, initial_text=''):
+        self.window.show_input_panel('New marker', initial_text, self.add_marker, None, None)
+
+    def list(self):
+        markers = self.load_markers()
+
+        def on_done(index):
+            if index == 0:
+                self.new_marker('')
+            elif index >= 1:
+                self.actions(markers[index - 1])
+
+        items = ["+ New"] + markers
+
+        sublime.set_timeout(lambda: self.window.show_quick_panel(items, on_done), 0)
+
+    def actions(self, item):
+        def on_done(index):
+            if index == 0:
+                self.list()
+            elif index == 1:
+                markers = self.load_markers()
+                markers.remove(item)
+                self.save_markers(markers)
+                self.list()
+
+        items = [['< Back', ''], ['- Delete', item]]
+        sublime.set_timeout(lambda: self.window.show_quick_panel(items, on_done), 0)
